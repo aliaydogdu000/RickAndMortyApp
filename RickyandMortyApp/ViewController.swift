83908e7eaf24491f553bdc608c723baff833ab06
Ali Aydoğdu <86653103+aliaydogdu000@users.noreply.github.com>
2022-09-24 12:25:15 +0300

    Initial Commit

---
+++ RickyandMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyandMortyApp
+//
+//  Created by Ali Aydoğdu on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
